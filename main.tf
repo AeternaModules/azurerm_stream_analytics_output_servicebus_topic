649b1fcd6be8fe422e9f3bcd768b7b6fc830846a
Daniel Allen <dan@aeternagroup.net>
2026-01-11 19:33:25 +0000

    Update to schema version v4.57.0

---
+++ main.tf
@@ -0,0 +1,22 @@
+resource "azurerm_stream_analytics_output_servicebus_topic" "stream_analytics_output_servicebus_topics" {
+  for_each = var.stream_analytics_output_servicebus_topics
+
+  name                      = each.value.name
+  resource_group_name       = each.value.resource_group_name
+  servicebus_namespace      = each.value.servicebus_namespace
+  stream_analytics_job_name = each.value.stream_analytics_job_name
+  topic_name                = each.value.topic_name
+  authentication_mode       = each.value.authentication_mode
+  property_columns          = each.value.property_columns
+  shared_access_policy_key  = each.value.shared_access_policy_key
+  shared_access_policy_name = each.value.shared_access_policy_name
+  system_property_columns   = each.value.system_property_columns
+
+  serialization {
+    encoding        = each.value.serialization.encoding
+    field_delimiter = each.value.serialization.field_delimiter
+    format          = each.value.serialization.format
+    type            = each.value.serialization.type
+  }
+}
+
---
+++ variables.tf
@@ -0,0 +1,42 @@
+variable "stream_analytics_output_servicebus_topics" {
+  description = <<EOT
+Map of stream_analytics_output_servicebus_topics, attributes below
+Required:
+    - name
+    - resource_group_name
+    - servicebus_namespace
+    - stream_analytics_job_name
+    - topic_name
+    - serialization (block):
+        - encoding (optional)
+        - field_delimiter (optional)
+        - format (optional)
+        - type (required)
+Optional:
+    - authentication_mode
+    - property_columns
+    - shared_access_policy_key
+    - shared_access_policy_name
+    - system_property_columns
+EOT
+
+  type = map(object({
+    name                      = string
+    resource_group_name       = string
+    servicebus_namespace      = string
+    stream_analytics_job_name = string
+    topic_name                = string
+    authentication_mode       = optional(string, "ConnectionString")
+    property_columns          = optional(list(string))
+    shared_access_policy_key  = optional(string)
+    shared_access_policy_name = optional(string)
+    system_property_columns   = optional(map(string))
+    serialization = object({
+      encoding        = optional(string)
+      field_delimiter = optional(string)
+      format          = optional(string)
+      type            = string
+    })
+  }))
+}
+
