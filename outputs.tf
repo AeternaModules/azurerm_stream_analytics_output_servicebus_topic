26e01f0f5e7341669662f7a4ab7ec221f3231da6
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:58 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,51 +1,46 @@
-output "stream_analytics_output_servicebus_topics" {
-  description = "All stream_analytics_output_servicebus_topic resources"
-  value       = azurerm_stream_analytics_output_servicebus_topic.stream_analytics_output_servicebus_topics
-  sensitive   = true
-}
 output "stream_analytics_output_servicebus_topics_authentication_mode" {
-  description = "List of authentication_mode values across all stream_analytics_output_servicebus_topics"
-  value       = [for k, v in azurerm_stream_analytics_output_servicebus_topic.stream_analytics_output_servicebus_topics : v.authentication_mode]
+  description = "Map of authentication_mode values across all stream_analytics_output_servicebus_topics, keyed the same as var.stream_analytics_output_servicebus_topics"
+  value       = { for k, v in azurerm_stream_analytics_output_servicebus_topic.stream_analytics_output_servicebus_topics : k => v.authentication_mode }
 }
 output "stream_analytics_output_servicebus_topics_name" {
-  description = "List of name values across all stream_analytics_output_servicebus_topics"
-  value       = [for k, v in azurerm_stream_analytics_output_servicebus_topic.stream_analytics_output_servicebus_topics : v.name]
+  description = "Map of name values across all stream_analytics_output_servicebus_topics, keyed the same as var.stream_analytics_output_servicebus_topics"
+  value       = { for k, v in azurerm_stream_analytics_output_servicebus_topic.stream_analytics_output_servicebus_topics : k => v.name }
 }
 output "stream_analytics_output_servicebus_topics_property_columns" {
-  description = "List of property_columns values across all stream_analytics_output_servicebus_topics"
-  value       = [for k, v in azurerm_stream_analytics_output_servicebus_topic.stream_analytics_output_servicebus_topics : v.property_columns]
+  description = "Map of property_columns values across all stream_analytics_output_servicebus_topics, keyed the same as var.stream_analytics_output_servicebus_topics"
+  value       = { for k, v in azurerm_stream_analytics_output_servicebus_topic.stream_analytics_output_servicebus_topics : k => v.property_columns }
 }
 output "stream_analytics_output_servicebus_topics_resource_group_name" {
-  description = "List of resource_group_name values across all stream_analytics_output_servicebus_topics"
-  value       = [for k, v in azurerm_stream_analytics_output_servicebus_topic.stream_analytics_output_servicebus_topics : v.resource_group_name]
+  description = "Map of resource_group_name values across all stream_analytics_output_servicebus_topics, keyed the same as var.stream_analytics_output_servicebus_topics"
+  value       = { for k, v in azurerm_stream_analytics_output_servicebus_topic.stream_analytics_output_servicebus_topics : k => v.resource_group_name }
 }
 output "stream_analytics_output_servicebus_topics_serialization" {
-  description = "List of serialization values across all stream_analytics_output_servicebus_topics"
-  value       = [for k, v in azurerm_stream_analytics_output_servicebus_topic.stream_analytics_output_servicebus_topics : v.serialization]
+  description = "Map of serialization values across all stream_analytics_output_servicebus_topics, keyed the same as var.stream_analytics_output_servicebus_topics"
+  value       = { for k, v in azurerm_stream_analytics_output_servicebus_topic.stream_analytics_output_servicebus_topics : k => v.serialization }
 }
 output "stream_analytics_output_servicebus_topics_servicebus_namespace" {
-  description = "List of servicebus_namespace values across all stream_analytics_output_servicebus_topics"
-  value       = [for k, v in azurerm_stream_analytics_output_servicebus_topic.stream_analytics_output_servicebus_topics : v.servicebus_namespace]
+  description = "Map of servicebus_namespace values across all stream_analytics_output_servicebus_topics, keyed the same as var.stream_analytics_output_servicebus_topics"
+  value       = { for k, v in azurerm_stream_analytics_output_servicebus_topic.stream_analytics_output_servicebus_topics : k => v.servicebus_namespace }
 }
 output "stream_analytics_output_servicebus_topics_shared_access_policy_key" {
-  description = "List of shared_access_policy_key values across all stream_analytics_output_servicebus_topics"
-  value       = [for k, v in azurerm_stream_analytics_output_servicebus_topic.stream_analytics_output_servicebus_topics : v.shared_access_policy_key]
+  description = "Map of shared_access_policy_key values across all stream_analytics_output_servicebus_topics, keyed the same as var.stream_analytics_output_servicebus_topics"
+  value       = { for k, v in azurerm_stream_analytics_output_servicebus_topic.stream_analytics_output_servicebus_topics : k => v.shared_access_policy_key }
   sensitive   = true
 }
 output "stream_analytics_output_servicebus_topics_shared_access_policy_name" {
-  description = "List of shared_access_policy_name values across all stream_analytics_output_servicebus_topics"
-  value       = [for k, v in azurerm_stream_analytics_output_servicebus_topic.stream_analytics_output_servicebus_topics : v.shared_access_policy_name]
+  description = "Map of shared_access_policy_name values across all stream_analytics_output_servicebus_topics, keyed the same as var.stream_analytics_output_servicebus_topics"
+  value       = { for k, v in azurerm_stream_analytics_output_servicebus_topic.stream_analytics_output_servicebus_topics : k => v.shared_access_policy_name }
 }
 output "stream_analytics_output_servicebus_topics_stream_analytics_job_name" {
-  description = "List of stream_analytics_job_name values across all stream_analytics_output_servicebus_topics"
-  value       = [for k, v in azurerm_stream_analytics_output_servicebus_topic.stream_analytics_output_servicebus_topics : v.stream_analytics_job_name]
+  description = "Map of stream_analytics_job_name values across all stream_analytics_output_servicebus_topics, keyed the same as var.stream_analytics_output_servicebus_topics"
+  value       = { for k, v in azurerm_stream_analytics_output_servicebus_topic.stream_analytics_output_servicebus_topics : k => v.stream_analytics_job_name }
 }
 output "stream_analytics_output_servicebus_topics_system_property_columns" {
-  description = "List of system_property_columns values across all stream_analytics_output_servicebus_topics"
-  value       = [for k, v in azurerm_stream_analytics_output_servicebus_topic.stream_analytics_output_servicebus_topics : v.system_property_columns]
+  description = "Map of system_property_columns values across all stream_analytics_output_servicebus_topics, keyed the same as var.stream_analytics_output_servicebus_topics"
+  value       = { for k, v in azurerm_stream_analytics_output_servicebus_topic.stream_analytics_output_servicebus_topics : k => v.system_property_columns }
 }
 output "stream_analytics_output_servicebus_topics_topic_name" {
-  description = "List of topic_name values across all stream_analytics_output_servicebus_topics"
-  value       = [for k, v in azurerm_stream_analytics_output_servicebus_topic.stream_analytics_output_servicebus_topics : v.topic_name]
+  description = "Map of topic_name values across all stream_analytics_output_servicebus_topics, keyed the same as var.stream_analytics_output_servicebus_topics"
+  value       = { for k, v in azurerm_stream_analytics_output_servicebus_topic.stream_analytics_output_servicebus_topics : k => v.topic_name }
 }
 
